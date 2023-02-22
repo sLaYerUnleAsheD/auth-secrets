971ea09071857adbe69128a7de07bba0b0b1ca0c
Mihir Panchal <mihirpanchal2603@gmail.com>
2023-02-22 20:54:52 +0530

    Level 3 security (password hashing with md5)

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt= require('mongoose-encryption');
+// commented out because in case of high level breach the .env file is vulnerable
+// so we use md5 to hash passwords instead
+// const encrypt= require('mongoose-encryption');
+const md5 = require("md5");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,7 +21,9 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
+// commented out because in case of high level breach the .env file is vulnerable
+// so we use md5 to hash passwords instead
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -38,7 +43,7 @@ app.post("/register", function(req, res) {
 
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
@@ -54,7 +59,7 @@ app.post("/register", function(req, res) {
 app.post("/login", function(req, res) {
 
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: username}, function(err, foundUser) {
         if(!err){
             if(foundUser){
