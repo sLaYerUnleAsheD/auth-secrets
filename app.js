b176fb3bb9196c4e64004d778b1382146669ff85
Mihir Panchal <mihirpanchal2603@gmail.com>
2023-02-23 13:02:25 +0530

    Passwords now go through Hashing and 10 rounds of salting (Bcrypt level 4 security)

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 // commented out because in case of high level breach the .env file is vulnerable
-// so we use md5 to hash passwords instead
+// so we use md5 to hash passwords instead (level 3 security)
 // const encrypt= require('mongoose-encryption');
-const md5 = require("md5");
+
+// commented out because brute force attack is still possible
+// to crack dictonary passwords so we will use bcrypt instead of md5
+// to hash and salt our passwords (level 4 security)
+// const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -41,31 +47,49 @@ app.get('/register', function(req, res) {
 
 app.post("/register", function(req, res) {
 
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-
-    newUser.save(function(err) {
-        if(!err){
-            res.render("secrets");
-        }else{
-            console.log(err);
-        }
-    });
+    // instead of md5(req.body.password); we use bcrypt's hash method
+    // and pass it user entered password and no. of salt rounds,
+    // in callback function it takes the final hash generated after
+    // mentioned no. of salt rounds which we can store in our database
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err) {
+            if(!err){
+                res.render("secrets");
+            }else{
+                console.log(err);
+            }
+        });
+    })
+    
 
 })
 
 app.post("/login", function(req, res) {
 
     const username = req.body.username;
-    const password = md5(req.body.password);
+    // const password = md5(req.body.password);
+    const password = req.body.password;
     User.findOne({email: username}, function(err, foundUser) {
         if(!err){
             if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }
+
+                // bcrypt.compare() takes in user entered password, second parameter
+                // is the hash that was generated for the foundUser, so we tap into the
+                // password field of foundUser which contains that hash in database
+                // compare() method will compare both and in the callback function the
+                // result will be passed which is boolean so we use it to check if they match
+                // or not.
+                
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                })
             }
         }else{
             console.log(err);
