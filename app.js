87ee367e504d7ae07e25e3f846c191b96243aaa6
Mihir Panchal <mihirpanchal2603@gmail.com>
2023-02-23 23:11:58 +0530

    Sign in/up with Google feature with Passport.js --(Level 6 Security), little styling changes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 // this is level 5 security using Passport.js, cookies & sessions.
 // to check out previous levels take a look at the previous commits.
@@ -22,10 +24,11 @@ app.set('view engine', 'ejs');
 
 //create session with options recommended by documentation
 app.use(session({
-    secret: "This is a secret.",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
 }));
+
 
 // initialize passport
 app.use(passport.initialize());
@@ -37,12 +40,14 @@ mongoose.connect("mongodb://0.0.0.0:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 // plugin passportLocalMongoose into our userSchema, it will perform
 // hashing and salting of passwords for us.
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
@@ -50,15 +55,55 @@ passport.use(User.createStrategy());
 
 // when we say serializeUser, passport creates the cookie
 // puts in the users identification.
-passport.serializeUser(User.serializeUser());
-
+passport.serializeUser(function(user, done) {
+   done(null, user.id);
+});
+  
 // when we say deserializeUser, passport extracts the info
 // from the cookie and authenticates the user.
-passport.deserializeUser(User.deserializeUser());
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
 
 app.get('/', function(req, res) {
     res.render("home");
 });
+
+// this below get method is present in passport.js google oauth 2.0 strategies doc
+// link: https://www.passportjs.org/packages/passport-google-oauth20/
+
+app.get('/auth/google', 
+    passport.authenticate('google', { scope: ['profile'] })
+);
+
+//after being authenticated the user will be redirected to the 
+//privileged page and we deal with that GET request below
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets page.
+    res.redirect('/secrets');
+  });
 
 app.get('/login', function(req, res) {
     res.render("login");
